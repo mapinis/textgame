30168660a8bef90c571a95a87ac8e6cf7ec70b96
Mark Apinis <mapinis2019@gmail.com>
2016-09-27 21:37:07 -0400

    finished place class

--- textgame.py
+++ textgame.py
@@ -1,9 +1,34 @@
 import random as rand
 
-class place():
+class Place(object):
     setting = 0
-    loot[] = 0
+    lootType = 0
+    healthBuff = 0
+    strengthBuff = 0
+    event = 0
+    done = 0
 
-    def __init__():
-        setting = ("cave", "forest", "fork", "camp")[rand.randint(0, 3)]
+    def generateLoot(self):
+        self.lootType = ("health potion", "strength potion", "surprise")[rand.randint(0, 2)]
+        if(self.lootType == "health potion"):
+            self.healthBuff = rand.randint(5, 30)
+        elif(self.lootType == "strength potion"):
+            self.strengthBuff = rand.randint(5, 30)
+    
+    def __init__(self):
+        self.setting = ("cave", "forest", "fork", "camp")[rand.randint(0, 3)]
+        if(self.setting == "fork"):
+            self.event == "choice"
+        elif(self.setting == "camp"):
+            self.event = "safety" if rand.randint(0, 100) <= 75 else "fight"
+            self.generateLoot()
+        else:
+            self.event = ("fight", "loot")[rand.randint(0, 1)]
+            self.generateLoot()
+
+        self.done = False
+    
+for x in range(0, 3):
+    place = Place()
+    print(place.setting, place.event, place.lootType, place.healthBuff, place.strengthBuff)
         
